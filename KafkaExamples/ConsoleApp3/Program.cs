5c1d75384370323c2a62e08b80baa7d1684282a0
Fred <admin@raspberryfield.life>
2020-08-03 15:15:14 +0200

    Can produce Kafka message in Protobuf format.

--- KafkaExamples/ConsoleApp3/Program.cs
+++ KafkaExamples/ConsoleApp3/Program.cs
@@ -1,28 +1,34 @@
 ﻿using Confluent.Kafka;
+using Google.Protobuf.Examples.Person;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! - ConsoleApp3 Kafka/protobuf.");
             Console.WriteLine("press any button to send message...");
             Console.ReadLine();
 
-            var config = new ProducerConfig { BootstrapServers = "172.18.82.14:9092"
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "172.18.82.14:9092"
+          
             };
 
+        
             // If serializers are not specified, default serializers from
             // `Confluent.Kafka.Serializers` will be automatically used where
             // available. Note: by default strings are encoded as UTF8.
-            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            using (var producer = new ProducerBuilder<Null, Person>(config).SetValueSerializer(new ProtoSerializer<Person>()).Build())
             {
                 try
                 {
-                    var dr = await p.ProduceAsync("my_first_topic", new Message<Null, string> { Value = "test2iii" });
+                    var dr = await producer.ProduceAsync("my_first_topic", new Message<Null, Person> { Value = new Person {Name = "Lena", Age = 34} });
                     Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
                 }
                 catch (ProduceException<Null, string> e)
@@ -31,7 +37,9 @@ namespace ConsoleApp1
                 }
             }
 
+
             Console.WriteLine("Exiting program...");
+
         }
     }
 }
---
+++ KafkaExamples/ConsoleApp3/ProtoSerializer.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using Google.Protobuf;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class ProtoSerializer<T> : ISerializer<T> where T : Google.Protobuf.IMessage<T>
+    {
+        public IEnumerable<KeyValuePair<string, object>>
+            Configure(IEnumerable<KeyValuePair<string, object>> config, bool isKey)
+                => config;
+
+        public void Dispose() { }
+
+        public byte[] Serialize(string topic, T data) //Deprecated?
+            => data.ToByteArray();
+
+        public byte[] Serialize(T data, SerializationContext context)
+            => data.ToByteArray();
+    }
+}
